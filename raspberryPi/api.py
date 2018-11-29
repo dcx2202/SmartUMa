8baec4e0710282515d8b3196426eb1cd49cd71be
Diogo Cruz <31130828+dcx2202@users.noreply.github.com>
2018-11-29 19:46:33 +0000

    Added a resource, database access and enabled CORS

--- raspberryPi/api.py
+++ raspberryPi/api.py
@@ -1,5 +1,7 @@
-from flask import Flask, request
+from flask import Flask, jsonify, request
 from flask_restful import Resource, Api
+import db_manager
+import raspbpi
 
 app = Flask(__name__)
 api = Api(app)
@@ -7,54 +9,70 @@ api = Api(app)
 
 class NumberOfCarsParked(Resource):
     def get(self):
-        # return {"Number of cars parked": get_number_of_cars_parked_from_db()}
-        return {"Number of cars parked": "es um pao"}
+        response = db_manager.get_num_cars_parked_from_db()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
 
 class NumberOfEntriesInTheLastHour(Resource):
     def get(self):
-        # return {"Number of entries in the last hour": get_number_of_entries_in_the_last_hour_from_db()} - retorna json
-        return
+        response = db_manager.get_num_entries_last_hour_from_db()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
 
 class NumberOfExitsInTheLastHour(Resource):
     def get(self):
-        # return {"Number of exits in the last hour": get_number_of_exits_in_the_last_hour_from_db()}
-        return
+        response = db_manager.get_num_exits_last_hour_from_db()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
 
 class NumberOfSpaces(Resource):
     def get(self):
-        # return {"Number of spaces": get_number_of_spaces()}
-        return
+        response = raspbpi.get_num_spaces()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
+
+
+class NumberOfFreeSpaces(Resource):
+    def get(self):
+        response = db_manager.get_num_free_spaces_from_db()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
 
 class Statistics(Resource):
     def get(self):
-        # return {"Statistics": get_statistics()}
-        return
+        response = raspbpi.get_statistics()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
 
 class Last24HoursHistory(Resource):
     def get(self):
-        # return {"Last 24 hours history": get_last_24_hours_history()}
-        return
+        response = db_manager.get_last_24h_log_from_database()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
 
 class AllTimeHistory(Resource):
     def get(self):
-        # return {"All time history": get_all_time_history()}
-        return
+        response = db_manager.get_full_log_from_database()
+        response.headers.add("Access-Control-Allow-Origin", "*")
+        return response
 
 
 api.add_resource(NumberOfCarsParked, '/number_of_cars_parked')
 api.add_resource(NumberOfEntriesInTheLastHour, '/number_of_entries_in_the_last_hour')
 api.add_resource(NumberOfExitsInTheLastHour, '/number_of_exits_in_the_last_hour')
 api.add_resource(NumberOfSpaces, '/number_of_spaces')
+api.add_resource(NumberOfFreeSpaces, '/number_of_free_spaces')
 api.add_resource(Statistics, '/statistics')
 api.add_resource(Last24HoursHistory, '/last_24_hours_history')
 api.add_resource(AllTimeHistory, '/all_time_history')
 
 
 if __name__ == '__main__':
-    app.run(debug=False, host='192.168.1.156') #change host ip if raspberry is on a different network
+    app.run(debug=False, host='0.0.0.0', port='80')
+
