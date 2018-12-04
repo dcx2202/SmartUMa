060b1898af275a1aeac1920ecd76ded3a5e9e8b3
unknown <marcopm1997@hotmail.com>
2018-12-04 13:22:14 +0000

    dashboard graph fix

--- dashboard/js/scripts.js
+++ dashboard/js/scripts.js
@@ -4,152 +4,10 @@
 var timeout_timer = 2000;
 var source_url = 'http://84.23.208.186:25000';
 
-/*
-function getNumberOfCars() {
-  $.ajax({
-    type: "get",
-    url: source_url + "/number_of_cars_parked",
-    success: function (data) {
-      //console.log the response
-      console.log(data);
-      $('#number_of_cars_parked').text(data);
-      getNumberOfFreeSpaces();
-    },
-    error: function () {
-      console.log('error');
-      getNumberOfFreeSpaces();
-    }
-  });
-}
-
-function getNumberOfFreeSpaces() {
-  $.ajax({
-    type: "get",
-    url: source_url + "/number_of_free_spaces",
-    success: function (data) {
-      //console.log the response
-      console.log(data);
-      $('#number_of_free_spaces').text(data);
-      getNumberOfEntriesInTheLastHour();
-    },
-    error: function () {
-      console.log('error');
-      getNumberOfEntriesInTheLastHour();
-    }
-  });
-}
-
-function getNumberOfEntriesInTheLastHour() {
-  $.ajax({
-    type: "get",
-    url: source_url + "/number_of_entries_in_the_last_hour",
-    success: function (data) {
-      //console.log the response
-      console.log(data);
-      $('#number_of_entries_last_hour').text(data);
-      getNumberOfExitsInTheLastHour();
-    },
-    error: function () {
-      console.log('error');
-      getNumberOfExitsInTheLastHour();
-    }
-  });
-}
-
-function getNumberOfExitsInTheLastHour() {
-  $.ajax({
-    type: "get",
-    url: source_url + "/number_of_exits_in_the_last_hour",
-    success: function (data) {
-      //console.log the response
-      console.log(data);
-      $('#number_of_exits_last_hour').text(data);
-      //Send another request in 2 seconds.
-      getNumberOfSpaces();
-    },
-    error: function () {
-      console.log('error');
-      getNumberOfSpaces();
-    }
-  });
-}
-
-function getNumberOfSpaces() {
-  $.ajax({
-    type: "get",
-    url: source_url + "/number_of_spaces",
-    success: function (data) {
-      //console.log the response
-      console.log(data);
-      $('#number_of_spaces').text(data);
-      getStatistics();
-    },
-    error: function () {
-      console.log('error');
-      getStatistics();
-    }
-  });
-}
-
-function getStatistics() {
-  $.ajax({
-    type: "get",
-    url: source_url + "/statistics",
-    success: function (data) {
-      //console.log the response
-      console.log(data);
-      $('#statistics').text(data);
-      get24hLog();
-    },
-    error: function () {
-      console.log('error');
-      get24hLog();
-    }
-  });
-}
-
-function get24hLog() {
-  $.ajax({
-    type: "get",
-    url: source_url + "/last_24_hours_history",
-    success: function (data) {
-      //console.log the response
-      console.log(data);
-      $('#24h_log').text(data);
-      getFullLog();
-    },
-    error: function () {
-      console.log('error');
-      getFullLog();
-    }
-  });
-}
-
-function getFullLog() {
-  $.ajax({
-    type: "get",
-    url: source_url + "/all_time_history",
-    success: function (data) {
-      //console.log the response
-      console.log(data);
-      $('#all_time_log').text(data);
-      setTimeout(function () {
-        getNumberOfCars();
-      }, 1000);
-    },
-    error: function () {
-      setTimeout(function () {
-        console.log('error');
-        getNumberOfCars();
-      }, timeout_timer);
-    }
-  });
-}
-
 function logout() {
   window.alert("logout!")
 }
-*/
+
 
 function getMainPackage() {
   $.ajax({
@@ -293,6 +151,5 @@ function updateGraph(data_array) {
 
 //Call functions
 
-//getNumberOfCars();
 drawGraph();
 getMainPackage();
